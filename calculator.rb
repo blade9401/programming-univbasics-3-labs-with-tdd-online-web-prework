883c9b619f424c6425da7a9e79806f0d1062282a
Eric murray <ericwmurray1994@gmail.com>
2019-12-05 05:36:12 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,4 +2,5 @@ first_number = 0
 second_number = 1 
 sum = second_number + first_number
 difference = first_number - second_number
-qotient = first_number ** second_number
+product = first_number ** second_number
+quotient = first_number / second_number
